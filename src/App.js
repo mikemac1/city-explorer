760e163a689b0803f833391e86f9a526cff150ce
Mike McCarty <mike.p.mccarty@gmail.com>
2022-11-16 14:41:20 -0800

    Completed todo number 3 for lab 6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 class App extends React.Component {
@@ -7,6 +9,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      modalShown: false,
       city: '',
       cityData: {},
       isError: false,
@@ -19,6 +22,13 @@ class App extends React.Component {
     })
   }
 
+  closeModal = () => {
+    console.log('Inside of close modal');
+    this.setState({
+      modalShown: false
+    });
+  };
+
   handleCitySubmit = async (e) => {
 
     let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
@@ -30,6 +40,7 @@ class App extends React.Component {
 
       this.setState({
         cityData: urlData.data[0],
+        modalShown: true,
         isError: false
       });
       console.log(this.state.cityData);
@@ -49,14 +60,14 @@ class App extends React.Component {
     let showLat = '';
     let showLong = '';
 
-    let showMap = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=13`;
+    let showMap = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=11`;
 
     if (this.state.isError) {
       showError = <h3>{this.state.errorMessage}</h3>
     } else {
-      showCity = <li>City: {this.state.cityData.display_name}</li>;
-      showLat = <li>Latitude: {this.state.cityData.lat}</li>;
-      showLong = <li>Longitude: {this.state.cityData.lon}</li>;
+      showCity = this.state.cityData.display_name;
+      showLat = this.state.cityData.lat;
+      showLong = this.state.cityData.lon;
     }
 
 
@@ -68,25 +79,37 @@ class App extends React.Component {
           </header>
           <main>
             <form onSubmit={this.handleCitySubmit}>
-              <label>Pick A City
-                <input name="city" type='text' onChange={this.handleCityInput} />
+              <label htmlFor="cityLink">Pick A City
+                <input id="cityLink" name="city" type='text' onChange={this.handleCityInput} />
               </label>
               <button type="submit" variant="success">Explore</button>
               <article>
                 {showError}
               </article>
-              <ul>
-                {showCity}
-                {showLat}
-                {showLong}
-              </ul>
+
             </form>
-            <img
-              src={showMap}
-              alt={this.state.city.name + ' area map.'}
-            />
           </main>
+          <Modal show={this.state.modalShown} onHide={this.closeModal}
+              size="lg"
+              aria-labelledby="contained-modal-title-vcenter"
+              centered
+            >
+              <Modal.Header>
+                <Modal.Title>City - {showCity}</Modal.Title>
+              </Modal.Header>
+
+              <Modal.Body>
+                <img src={showMap} alt={this.state.city.name + ' area map.'} />
+                <h3>Latitude: {showLat}</h3>
+                <h3>Longitude: {showLong}</h3>
+              </Modal.Body>
+
+              <Modal.Footer>
+                <Button onClick={this.closeModal} variant="secondary">Close</Button>
+              </Modal.Footer>
+            </Modal>
           <footer>
+            {/* <p><span>&copy</span>  Mike McCarty</p> */}
           </footer>
         </body>
       </>
